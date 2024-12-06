5cf2055c1f53456ea80143e0072b601850696174
mahdiyusofi <125895658+mahdiyusofi@users.noreply.github.com>
2024-12-06 11:25:55 +0100

    v1.1

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const onbtn = document.querySelector("#hamburger-show-btn");
 onbtn.addEventListener("click", () => {
     offscreenMenu.classList.toggle("active");
 })
+
+function myFunction() {
+    document.getElementById("lan-Dropdown").classList.toggle("show");
+  }
+
+window.onclick = function(event) {
+    if (!event.target.matches('.lan-dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
